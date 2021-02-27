0f0f78e89aa6468750298bc2103031b4c4a8539e
Nate <7nater.nelson@gmail.com>
2021-02-26 17:40:27 -0700

    commit tree e3f9762fd38f8a0f2ac64978f567963fa2111a19

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
+require 'pry'
 class Helpers
+    def self.current_user(sess)
+        User.find_by(id: sess[:user_id])
+    end
+
+    def self.is_logged_in?(sess)
+        !!sess[:user_id]
+    end
 end
